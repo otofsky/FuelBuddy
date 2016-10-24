958642fd39cc4b3854ce1a6d8ea7699c24200ac1
zjuroszek <zjuroszek@mobiltek.pl>
2016-10-24 17:03:39 +0200

    podmiana aktywności startowej

--- presentation/src/main/java/com/fuelbuddy/mobile/home/HomeActivity.java
+++ presentation/src/main/java/com/fuelbuddy/mobile/home/HomeActivity.java
@@ -1,10 +1,21 @@
 package com.fuelbuddy.mobile.home;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.fuelbuddy.mobile.R;
 
 /**
  * Created by zjuroszek on 07.10.16.
  */
 public class HomeActivity extends Activity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
 
 }
