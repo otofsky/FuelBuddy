7b1e5042da2312d9e7236e6d4d489cf5f62eed12
zjuroszek <zjuroszek@mobiltek.pl>
2016-12-01 16:21:13 +0100

    dodanie metody sprawdzajcej czy uzytkowni kjuz instenije

--- domain/src/main/java/com/fuelbuddy/interactor/GetCurrentUser.java
+++ domain/src/main/java/com/fuelbuddy/interactor/GetCurrentUser.java
@@ -3,17 +3,13 @@ package com.fuelbuddy.interactor;
 import com.fuelbuddy.data.User;
 import com.fuelbuddy.executor.PostExecutionThread;
 import com.fuelbuddy.executor.ThreadExecutor;
-import com.fuelbuddy.repository.GasStationsRepository;
 import com.fuelbuddy.repository.UserRepository;
 
 
 import javax.inject.Inject;
 
-import hugo.weaving.DebugLog;
 import rx.Observable;
-import rx.Subscriber;
 import rx.functions.Func1;
-import rx.schedulers.Schedulers;
 
 /**
  * Created by zjuroszek on 14.11.16.
@@ -31,25 +27,40 @@ public class GetCurrentUser extends UseCase {
 
     @Override
     protected Observable buildUseCaseObservable() {
-        getUserObservable().concatMap(validateUser);
+        getLocalUserObservable().concatMap(validateRemoteUser);
         return userRepository.getCurrentUser();
     }
 
-
-    private final Func1<User, Observable<User>> validateUser =
+    private final Func1<User, Observable<User>> validateLocalUser =
             new Func1<User, Observable<User>>() {
                 @Override
                 public Observable<User> call(User user) {
                     if (user == null) {
-                        return userRepository.getCheckUser("1");
+                        return userRepository.getCheckUser("43");
                     } else {
                         return getUser(user);
                     }
                 }
             };
 
-    protected Observable getUserObservable() {
+    private final Func1<User, Observable<User>> validateRemoteUser =
+            new Func1<User, Observable<User>>() {
+                @Override
+                public Observable<User> call(User user) {
+                    if (user == null) {
+                        return userRepository.getCheckUser("43");
+                    } else {
+                        return getUser(user);
+                    }
+                }
+            };
+
+    protected Observable getLocalUserObservable() {
         return userRepository.getCurrentUser();
+    }
+
+    protected Observable getRemoteUserObservable(String userId) {
+        return userRepository.getCheckUser(userId);
     }
 
     public Observable<User> getUser(User user) {
